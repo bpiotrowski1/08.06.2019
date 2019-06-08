d7df1c48239a16067dd4e5876b12595787f681df
bpiotrowski1 <baej.p26@gmail.com>
2019-06-08 10:16:33 +0200

    modified list

--- src/main/java/pl/piotrowskib/Main.java
+++ src/main/java/pl/piotrowskib/Main.java
@@ -8,32 +8,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
-        System.out.print("Podaj liczbe elementow: ");
+        System.out.print("Podaj liczbe elementow do wylosowania: ");
         int n = in.nextInt();
         for (int i = 0; i < n; i++) {
-            list.add(in.nextInt());
-        }
-        for(int i=0; i<5;i++) {
             list.add(rand.nextInt(100));
         }
 
-        wypisz(list);
+        System.out.print("Podaj liczbe elementow do wczytania: ");
+        n = in.nextInt();
+        System.out.println("Podaj liczby do wczytania: ");
+        for (int i = 0; i < n; i++) {
+            list.add(in.nextInt());
+        }
+
+        System.out.println(list);
         wypiszSrednia(list);
     }
 
-    private static void wypisz(List list) {
-        for (Object o : list) {
-            System.out.print(o + " ");
-        }
-        System.out.println();
-    }
-
     private static void wypiszSrednia(List<Integer> list) {
-        Integer suma = 0;
+        double suma = 0;
         for (Integer o : list) {
             suma += o;
         }
